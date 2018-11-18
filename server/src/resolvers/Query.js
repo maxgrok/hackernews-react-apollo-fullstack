b1e2f6d4675da14e196d1c393269c4453a7dcb77
Max Goodman <max.goodman@flatironschool.com>
2018-11-18 14:56:35 -0500

    finished the fullstack graphql tutorial

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -4,13 +4,10 @@ async function feed(parent, args, ctx, info) {
     ? { OR: [{ url_contains: filter }, { description_contains: filter }] }
     : {}
 
-  const allLinks = await ctx.db.query.links({})
-  const count = allLinks.length
-
-  const queriedLinkes = await ctx.db.query.links({ first, skip, where })
+  const queriedLinks = await ctx.db.query.links({ first, skip, where })
 
   return {
-    linkIds: queriedLinkes.map(link => link.id),
+    linkIds: queriedLinks.map(link => link.id),
     count
   }
 }
